7a42ff6d653c1439728a96c9a8f35dad080c1b5a
unknown <joshc0044@gmail.com>
2019-09-17 16:38:05 -0400

    trying to make a plain dropdown

---
+++ lab/webapp/src/components/FileUpload/components/PlainDropdown/index.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Segment } from 'semantic-ui-react';
+
+class PlainDropdown extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidUpdate(prevProps) {
+    const { options } = this.props;
+    //window.console.log('old options', prevProps.options);
+    //window.console.log('new options', options);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //window.console.log('old options', this.props.options);
+    //window.console.log('new options', nextProps.options);
+    return true;
+  }
+
+  render() {
+    const { options, fieldType, dropdownHandler, multiple } = this.props;
+    window.console.log('PlainDropdown', options);
+    let tempOpts = [];
+    options.forEach((key, i) => {
+    tempOpts.push(
+      <option
+       key={key + '_' + i}
+       value={key}
+      >
+       {key}
+      </option>)
+    });
+    return (
+      <div>
+        <label>
+          PlainDropdown {fieldType}:
+        </label>
+        <Segment
+          inverted
+          compact
+        >
+          <select
+            style={{
+              width: '100%'
+            }}
+            name={'plain_dropdown_' + fieldType}
+            multiple={multiple}
+            onChange={dropdownHandler}
+          >
+            <option value="" disabled>--Please select {fieldType}--</option>
+            {tempOpts}
+          </select>
+        </Segment>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({});
+
+export { PlainDropdown };
+export default connect(mapStateToProps)(PlainDropdown);
